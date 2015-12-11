927d01f1124fec7af4d644707c1fe91a8f98e161
mickeroo <michael.eaton@oregonstate.edu>
2015-12-11 14:59:31 -0800

    Adding test for hidden SMS fields.

---
+++ spec/sms_fields_hidden.rb
@@ -0,0 +1,43 @@
+# https://github.com/osulp/primo/issues/6
+#
+# Hide the SMS label and input box in "My Account"
+
+require 'selenium-webdriver'
+require 'rspec'
+require 'spec_helper'
+
+describe 'On My Account > Personal Settings form' do
+
+  before(:context) do
+    @driver = Selenium::WebDriver.for :firefox
+    # @base_url = 'http://search.library.oregonstate.edu/'
+    @base_url = 'http://alliance-primo-sb.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?vid=OSU' # Primo Sandbox
+    @driver.manage.timeouts.implicit_wait = 15
+
+    # Run this as an ONID user.
+    @driver.get @base_url
+    @driver.find_element(:link, 'Current OSU students, faculty and staff').click
+    @driver.find_element(:id, 'username').send_keys ENV['user']
+    @driver.find_element(:id, 'password').send_keys ENV['pass']
+    @driver.find_element(:name, 'submit').click
+
+    # Bring up the My Account tab and edit form.
+    @driver.find_element(:link, 'My Account').click
+    @driver.find_element(:id, 'exlidMyAccountMenu1-5').click
+    @driver.find_element(:id, 'editPersonalDetails').click
+  end
+  
+  after(:context) do
+    @driver.quit
+  end
+
+  context 'for ONID authenticated user' do
+    it '"SMS" label is hidden' do
+      test_hidden_element(:xpath, "//table[@id='ilsFields']/tbody/tr[8]/th/label")
+    end
+    it '"SMS" input is hidden' do
+      test_hidden_element(:xpath, "//table[@id='ilsFields']/tbody/tr[8]/th/label")
+    end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,13 @@ RSpec.configure do |config|
   config.add_formatter :documentation
 end
 
+def test_hidden_element(type, value)
+  # These should not be present in the DOM.  If they are present, the .displayed? will fail the test.
+  begin
+    expect(@driver.find_element(type, value).displayed?).to_not be
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    # This is actually what we want, so this is a pass.
+    return true
+  end
+end
+
